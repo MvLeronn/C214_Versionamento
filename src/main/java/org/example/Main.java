a7224c196be45d9201e767590290b1c650862f54
LucaFelipe <luca.felipe@ges.inatel.br>
2025-02-25 22:41:53 -0300

    conflito certo  na main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,11 @@ public class Main {
         double[][] array = {{4, 7}, {3, 4}};
         Matrix A = new Matrix(array);
 
-        Matrix D = A.inverse();  // Inversa de A
-        Matrix C = A.transpose();  // Transposta de A
+        Matrix B = A.inverse();  // Inversa de A
+        Matrix C = A.inverse();  // Transposta de A
 
         System.out.println("Inversa de A:");
-        D.print(5, 2);
+        B.print(5, 2);
 
         System.out.println("Transposta de A:");
         C.print(5, 2);
