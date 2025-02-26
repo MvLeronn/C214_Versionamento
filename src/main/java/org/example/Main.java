7c3afdb8b772c90bd6a7b1141fbb1e5ca9ece326
Marcos Vinicius <marcos23_correia@hotmail.com>
2025-02-25 22:08:39 -0300

    add: Estrutura projeto matriz

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+import Jama.Matrix;
+
+public class Main {
+    public static void main(String[] args) {
+        double[][] array = {{4, 7}, {3, 4}};
+        Matrix A = new Matrix(array);
+
+        Matrix B = A.inverse();  // Inversa de A
+        Matrix C = A.transpose();  // Transposta de A
+
+        System.out.println("Inversa de A:");
+        B.print(5, 2);
+
+        System.out.println("Transposta de A:");
+        C.print(5, 2);
+
+        System.out.println("Determinante: " + A.det());
+    }
+}
